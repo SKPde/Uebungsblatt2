99f1b04f37dc1680054a006549bee39e32964d36
s.kuebel <s.kuebel@otterbach.de>
2017-08-03 13:38:32 +0200

    Weiter ergänzt, leider noch nicht die Aufgabe ganz verstanden

--- src/DiffTest.java
+++ src/DiffTest.java
@@ -3,7 +3,18 @@ public class DiffTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Halihalo!");
+		double wertX = 18;
+	}
+	
+	public Diff f(double x) {
+		
+		double f = x*(4+x)/(3-x);
+		double fstrich = ((-2)*x-x*x+12)/(9-6*x+x*x);
+		
+		Diff ausgabe = Diff.diffVar(f);
+		
+		return ausgabe;
+				
 	}
 
 }
